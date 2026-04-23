c8ca05944be31157aa83ab19a6c8ddb127ef51c5
MohamedHamdy1987 <shahat10101048@gmail.com>
2026-04-23 13:26:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,79 @@
-// TEST MODE
+import { renderDashboard } from "./pages/dashboard.js";
+import { renderInvoicesPage } from "./pages/invoices.js";
+import { renderSalesPage } from "./pages/sales.js";
+import { renderSuppliersPage } from "./pages/suppliers.js";
+import { renderCustomersPage } from "./pages/customers.js";
+import { renderTarhilPage } from "./pages/tarhil.js";
+import { renderKhaznaPage } from "./pages/khazna.js";
+import { renderEmployeesPage } from "./pages/employees.js";
+import { renderMarketShopsPage } from "./pages/market_shops.js";
 
-window.navigate = function(page){
 
-document.getElementById("app").innerHTML =
-"<h2>الزر اشتغل: " + page + "</h2>";
+const routes={
+
+dashboard:renderDashboard,
+
+invoices:renderInvoicesPage,
+
+sales:renderSalesPage,
+
+suppliers:renderSuppliersPage,
+
+customers:renderCustomersPage,
+
+tarhil:renderTarhilPage,
+
+khazna:renderKhaznaPage,
+
+employees:renderEmployeesPage,
+
+market_shops:renderMarketShopsPage
 
 };
 
-window.onload = function(){
 
-document.getElementById("app").innerHTML =
-"<h2>TEST MODE READY</h2>";
+window.navigate=
+async function(page){
+
+const app=
+document.getElementById("app");
+
+try{
+
+if(!routes[page]){
+
+app.innerHTML=
+"صفحة غير موجودة";
+
+return;
+
+}
+
+app.innerHTML=
+"جار التحميل...";
+
+await routes[page](app);
+
+}
+
+catch(e){
+
+console.error(e);
+
+app.innerHTML=
+"خطأ داخل الصفحة";
+
+}
+
+};
+
+
+// بدون auth حالياً
+window.onload=
+function(){
+
+navigate(
+"dashboard"
+);
 
 };
